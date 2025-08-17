f09d3acf9775802c2e0fee3cc169b23f244e091b
mikepenz <1476232+mikepenz@users.noreply.github.com>
2025-08-17 17:59:52 +0000

    Update adbfriend to 2.0.0-a01

--- Formula/adbfriend.rb
+++ Formula/adbfriend.rb
@@ -1,9 +1,9 @@
 class Adbfriend < Formula
     desc "Android ADB CLI tool with common actions used during development"
     homepage "https://github.com/mikepenz/adbfriend/"
-    url "https://github.com/mikepenz/adbfriend/releases/download/1.8.2/adbfriend-cli-shadow-1.8.2.zip"
-    sha256 "8f2b05d786ae47a91c99e27c3564b6f71c3bc0c95ef1acc7afc8a833b9320354"
-    version "1.8.2"
+    url "https://github.com/mikepenz/adbfriend/releases/download/2.0.0-a01/adbfriend-cli-shadow-2.0.0-a01.zip"
+    sha256 "3d30905a75d3e6f63bbb12ef2f49681911402a6fc2ef68f3caa9be138658d1be"
+    version "2.0.0-a01"
     license "Apache-2.0"
     def install
         rm_f Dir["bin/*.bat"]
