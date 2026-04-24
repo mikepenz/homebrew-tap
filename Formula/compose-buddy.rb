94caf1aa58df1fce3d8a359da404e01e9da08373
mikepenz <1476232+mikepenz@users.noreply.github.com>
2026-04-24 09:48:04 +0000

    Update compose-buddy to v0.3.0

--- Formula/compose-buddy.rb
+++ Formula/compose-buddy.rb
@@ -1,9 +1,9 @@
 class ComposeBuddy < Formula
     desc "CLI for rendering and inspecting Jetpack Compose @Preview composables"
     homepage "https://github.com/mikepenz/compose-buddy/"
-    url "https://github.com/mikepenz/compose-buddy/releases/download/v0.3.0-a04/compose-buddy-cli-v0.3.0-a04.zip"
-    sha256 "7888b6b2a8d90032de5bece5a110156bdf837e3e68b80e8e8332f57a373d119a"
-    version "v0.3.0-a04"
+    url "https://github.com/mikepenz/compose-buddy/releases/download/v0.3.0/compose-buddy-cli-v0.3.0.zip"
+    sha256 "b2af510c440e9d79af61fac3892ece2dc47f597e6c6c41f0fbf766c72e2e9cd6"
+    version "v0.3.0"
     license "Apache-2.0"
     depends_on "openjdk"
     def install
